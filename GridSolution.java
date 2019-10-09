bc612323d5cb236473fbda5512dd4cb339d7be15
William Broach <william.broach@gmail.com>
2019-10-09 14:26:50 -0400

    added GridSolution

---
+++ GridSolution.java
@@ -0,0 +1,50 @@
+
+
+public class GridSolution {
+
+    
+    private static boolean isMatch(char[] word, int rowIdx, int colIdx, int rowInc, int colInc, int place, int[][] grid) {
+	if (place == word.length) {
+	    return true; 
+	}
+	else if (rowIdx < 0 || rowIdx >= grid.length) {
+	    return false; 
+	}
+	else if (colIdx < 0 || colIdx >= grid[0].length) {
+	    return false; 
+	}
+	else if (word[place] != grid[rowIdx][colIdx]) {
+	    return false; 
+	}
+	else {
+	    return isMatch(word, rowIdx + rowInc, colIdx + colInc, rowInc, colInc, place + 1, grid);
+	}
+
+    }
+
+    public static void findMatches(String word, char[][] grid) {
+	char[] wordArr = word.toCharArray();
+
+	for (int i = 0; i < grid.length; ++i) {
+	    for (int j = 0; j < grid[0].length; ++j) {
+
+		if (grid[i][j] == wordArr[0]) { // found the first letter, so check in all directions
+		    boolean up = isMatch(wordArr, i - 1, j, -1, 0, 1, grid); 
+		    boolean upRight = isMatch(wordArr, i - 1, j + 1, -1, 1, 1, grid); 
+		    boolean right = isMatch(wordArr, i, j + 1, 0, 1, 1, grid); 
+		    boolean downRight = isMatch(wordArr, i + 1, j + 1, 1, 1, 1, grid); 
+		    boolean down = isMatch(wordArr, i + 1, j, 1, 0, 1, grid); 
+		    boolean downLeft = isMatch(wordArr, i + 1, j - 1, 1, -1, 1, grid); 
+		    boolean left = isMatch(wordArr, i, j - 1, 0, -1, 1, grid); 
+		    boolean upLeft = = isMatch(wordArr, i - 1, j - 1, -1, -1, 1, grid); 
+
+		    if (up || upRight || right || downRight || down || downLeft || left || upLeft) {
+			System.out.println("Pattern found at: (" + i + ", " + j + ")");  
+		    }
+		}
+
+	    }
+
+    }
+
+}
---
+++ prev_readmes/README.md
@@ -0,0 +1,22 @@
+# CSIP 09/18/2019
+
+Please [Check in here](https://forms.gle/YGcLj5aP7WoRwNbCA)
+
+## 1301 / 1302 / Taking Data Structures
+
+1) Given a Binary Search Tree, print the values in order
+2) Write two functions: one to count the number of nodes, and another to count the number of leaf nodes
+3) Write a function that returns the number of levels in a binary tree. The number of levels is equal to the furthest level reached by any leaf node in the tree. 
+
+
+## Has Taken Data Structures
+
+1) Write a function to insert into a BST.
+2) [Sum of Root To Leaf Binary Numbers](https://www.geeksforgeeks.org/sum-numbers-formed-root-leaf-paths/)
+3) Invert a binary tree   
+
+## Bonus
+
+1) Given a binary tree (represented as an array under the hood), write a method that turns this tree
+   into a max heap. You may assume that the binary tree has been filled left-to-right
+   
